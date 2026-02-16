fb7431ca863f6e9aff6c377a9ec5a6220dcddb43
mo0org <philippemorgane2@gmail.com>
2026-02-16 20:04:24 +0100

    adding script as exercise:)

---
+++ r_scripts/practice_script.R
@@ -0,0 +1,14 @@
+#uploading a .csv dataset
+data<-read.csv(here::here("data/raw_data/raw_data_1.csv"),sep=";",dec=",")
+
+#creating a subset
+subset<-data[1:3,2:3]
+
+#creating a dataset saved as .csv from the subset
+write.csv(subset,here::here("data/clean_data/subset_1.csv"))
+
+
+#creating a graph and saving it to outputs
+pdf(here::here("outputs/figures","plot1.pdf"))
+plot(1,1)
+dev.off()
